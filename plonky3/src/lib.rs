c3d8efa8a6ff02a00ceec74e830f3bcbd66563a4
MarcoOl94 <marco.olivero94@libero.it>
2025-06-16 14:40:45 +0200

    [P3] Moved p3-dependencies under a specific feature.
      "p3-field-testing",
      "p3-dft",
      "p3-monty-31",
      "p3-baby-bear",
      "p3-symmetric",
      "p3-poseidon2",

--- plonky3/src/lib.rs
+++ plonky3/src/lib.rs
@@ -1,8 +1,14 @@
+#[cfg(feature = "full-p3")]
 pub extern crate p3_baby_bear;
+#[cfg(feature = "full-p3")]
 pub extern crate p3_dft;
 pub extern crate p3_field;
+#[cfg(feature = "full-p3")]
 pub extern crate p3_field_testing;
 pub extern crate p3_goldilocks;
+#[cfg(feature = "full-p3")]
 pub extern crate p3_monty_31;
+#[cfg(feature = "full-p3")]
 pub extern crate p3_poseidon2;
+#[cfg(feature = "full-p3")]
 pub extern crate p3_symmetric;
